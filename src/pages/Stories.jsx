6f34df44cedd6f57f2bcc727bad971e0f6f47d97
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-10-10 12:22:14 +0300

    layoutGrid in storiesList

--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -4,15 +4,26 @@ import { slice } from "lodash";
 //Stories
 import animalStories from "../stories";
 
+//Components
 import Story from "../components/Story";
 
+//Images
+import grid from "../assets/icons/grid.png";
+import details from "../assets/icons/details.png";
+
 const Stories = () => {
+  //Grid Functionality
+  const [isGrid, setIsGrid] = useState(false);
+
+  //Load More Functionality
+  const initialStories = isGrid ? 12 : 5;
   const [isCompleted, setIsCompleted] = useState(false);
-  const [index, setIndex] = useState(5);
+  const [index, setIndex] = useState(initialStories);
   const stories = slice(animalStories, 0, index);
 
   const loadMore = () => {
-    setIndex(index + 5);
+    isGrid ? setIndex(index + 12) : setIndex(index + 5);
+
     if (index >= animalStories.length) {
       setIsCompleted(true);
     } else {
@@ -23,7 +34,21 @@ const Stories = () => {
   return (
     <div className="stories">
       <h1>Οι Ιστορίες μας</h1>
-      <div className="storiesList">
+      <div className="layoutBtns">
+        <button
+          className={`details ${isGrid ? "active" : ""}`}
+          onClick={() => setIsGrid(!isGrid)}
+        >
+          <img src={details} alt="Details" />
+        </button>
+        <button
+          className={`grid ${isGrid ? "" : "active"}`}
+          onClick={() => setIsGrid(!isGrid)}
+        >
+          <img src={grid} alt="Grid" />
+        </button>
+      </div>
+      <div className={`storiesList ${isGrid ? "gridList" : "detailsList"}`}>
         {stories.map((story) => (
           <Story key={story.id} story={story} />
         ))}
