08daeed1d9b20b2672a382e949cffaf1e86c3a17
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-10-10 17:21:05 +0300

    animals background

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Stories from "./pages/Stories";
+import AnimalsBackground from "./components/AnimalsBackground";
 
 function App() {
   return (
-    <div className="row">
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/stories" element={<Stories />} />
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      <AnimalsBackground />
+      <div className="row">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/stories" element={<Stories />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/AnimalsBackground.jsx
@@ -0,0 +1,97 @@
+import { motion } from "framer-motion";
+//Import animal images
+import {
+  koala,
+  cat,
+  cow,
+  fox,
+  lion,
+  panda,
+  turtle,
+  dog,
+  horse,
+  dove,
+} from "../animalImages";
+
+const AnimalsBackground = () => {
+  const variants = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 1, delay: 5 } },
+  };
+  return (
+    <div className="animalsBg">
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={koala}
+        alt="koala"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={cat}
+        alt="cat"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={cow}
+        alt="cow"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={fox}
+        alt="fox"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={lion}
+        alt="lion"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={panda}
+        alt="panda"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={turtle}
+        alt="turtle"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={dog}
+        alt="dog"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={horse}
+        alt="horse"
+      />
+      <motion.img
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        src={dove}
+        alt="dove"
+      />
+    </div>
+  );
+};
+
+export default AnimalsBackground;
