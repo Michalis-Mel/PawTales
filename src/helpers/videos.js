9de38c1b33fb078f45a9bb00e997b6d0dd0507c8
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-01-16 17:00:20 +0200

    changed christmas mp4

--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,14 +1,14 @@
-import pawtalesVideo from "../assets/homepage/songs/pawtalesSong_animation.mp4";
-import pawtalesChristmasVideo from "../assets/homepage/songs/ChristmasVideo.mp4";
+import pawtalesVideo from '../assets/homepage/songs/pawtalesSong_animation.mp4';
+import pawtalesChristmasVideo from '../assets/homepage/songs/christmas.mp4';
 
 const videos = [
   {
     src: pawtalesVideo,
-    alt: "Pawtales Video",
+    alt: 'Pawtales Video',
   },
   {
     src: pawtalesChristmasVideo,
-    alt: "Pawtales Christmas Video",
+    alt: 'Pawtales Christmas Video',
   },
 ];
 
