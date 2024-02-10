ebe1a1d97e7c36d73e086a2ff5cde9ef915633f3
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-10 21:31:23 +0200

    server configuration

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import history from 'express-history-api-fallback';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,10 +14,13 @@ const port = 5173;
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Handle all routes by serving the main HTML file
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// Use express-history-api-fallback middleware
+app.use(
+  history({
+    index: '/index.html',
+    verbose: true,
+  })
+);
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is listening on all network interfaces on port ${port}`);
--- src/components/UploadStory.jsx
+++ src/components/UploadStory.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { setDoc, doc } from "firebase/firestore";
-import { firestore } from "../helpers/firebase";
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { setDoc, doc } from 'firebase/firestore';
+import { firestore } from '../helpers/firebase';
 
 const uploadStoryToFirestore = async (storyData) => {
   try {
     // Format the date as "DD-MM-YYYY"
     const formattedDate = storyData.dateCreated
-      ? storyData.dateCreated.toLocaleDateString("en-GB", {
-          day: "numeric",
-          month: "numeric",
-          year: "numeric",
+      ? storyData.dateCreated.toLocaleDateString('en-GB', {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
         })
       : null;
 
@@ -21,28 +21,28 @@ const uploadStoryToFirestore = async (storyData) => {
     };
 
     await setDoc(
-      doc(firestore, "stories", storyData.id),
+      doc(firestore, 'stories', storyData.id),
       dataWithFormattedDate
     );
     return true;
   } catch (error) {
-    console.error("Error uploading story:", error);
+    console.error('Error uploading story:', error);
     return false;
   }
 };
 
 const UploadStory = () => {
   // State for form fields
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [audio, setAudio] = useState("");
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [audio, setAudio] = useState('');
   const [dateCreated, setDateCreated] = useState(new Date());
-  const [id, setId] = useState(uuidv4());
-  const [image, setImage] = useState("");
-  const [secondImage, setSecondImage] = useState("");
-  const [thirdImage, setThirdImage] = useState("");
-  const [smallDescription, setSmallDescription] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [id, setId] = useState('');
+  const [image, setImage] = useState('');
+  const [secondImage, setSecondImage] = useState('');
+  const [thirdImage, setThirdImage] = useState('');
+  const [smallDescription, setSmallDescription] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleUpload = async (e) => {
     e.preventDefault();
@@ -62,41 +62,51 @@ const UploadStory = () => {
 
     if (success) {
       // Clear form fields after successful upload
-      setTitle("");
-      setContent("");
-      setAudio("");
+      setTitle('');
+      setContent('');
+      setAudio('');
       setDateCreated(new Date());
-      setId(uuidv4());
-      setImage("");
-      setSecondImage("");
-      setThirdImage("");
-      setSmallDescription("");
-      setErrorMessage("");
+      setId('');
+      setImage('');
+      setSecondImage('');
+      setThirdImage('');
+      setSmallDescription('');
+      setErrorMessage('');
 
-      alert("Story uploaded successfully!");
+      alert('Story uploaded successfully!');
     } else {
-      setErrorMessage("Failed to upload story. Please try again.");
+      setErrorMessage('Failed to upload story. Please try again.');
     }
   };
 
   return (
-    <div className="upload-story-container">
+    <div className='upload-story-container'>
       {errorMessage && (
-        <div className="error-message">
+        <div className='error-message'>
           <h3>{errorMessage}</h3>
         </div>
       )}
-      <div className="upload-story-form">
+      <div className='upload-story-form'>
         <h1>Ανέβασμα Ιστορίας</h1>
         <form onSubmit={handleUpload}>
           <label>
             Title:
             <input
-              type="text"
+              type='text'
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
+            />
+          </label>
+          <label>
+            Url:
+            <input
+              type='text'
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+              className='form-input'
             />
           </label>
           <label>
@@ -105,46 +115,46 @@ const UploadStory = () => {
               value={content}
               onChange={(e) => setContent(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
             />
           </label>
           <label>
             Audio URL:
             <input
-              type="text"
+              type='text'
               value={audio}
               onChange={(e) => setAudio(e.target.value)}
-              className="form-input"
+              className='form-input'
             />
           </label>
           <label>
             Image URL:
             <input
-              type="text"
+              type='text'
               value={image}
               onChange={(e) => setImage(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
             />
           </label>
           <label>
             Second Image URL:
             <input
-              type="text"
+              type='text'
               value={secondImage}
               onChange={(e) => setSecondImage(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
             />
           </label>
           <label>
             Third Image URL:
             <input
-              type="text"
+              type='text'
               value={thirdImage}
               onChange={(e) => setThirdImage(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
             />
           </label>
           <label>
@@ -153,10 +163,10 @@ const UploadStory = () => {
               value={smallDescription}
               onChange={(e) => setSmallDescription(e.target.value)}
               required
-              className="form-input"
+              className='form-input'
             />
           </label>
-          <button type="submit" className="form-button">
+          <button type='submit' className='form-button'>
             Upload Story
           </button>
         </form>
