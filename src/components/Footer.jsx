52cc9ece2567225333e57ff760cf25739abf8464
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-11 01:23:31 +0200

    added instagram link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
+import instagram from '../assets/icons/instagram.png';
 const Footer = () => {
   let currentYear = new Date().getFullYear();
 
   return (
     <footer>
+      <a
+        href='https://www.instagram.com/paw___tales/'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='social'
+      >
+        Βρείτε μας στο Ιnstagram
+        <img src={instagram} alt='Instagram' />
+      </a>
       <span>&copy; Copyright {currentYear}, M² WebWorks</span>
     </footer>
   );
