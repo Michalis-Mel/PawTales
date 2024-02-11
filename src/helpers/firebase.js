f9aa3fc153caee9d27d81196b129ff0080795b1e
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-11 17:34:01 +0200

    redeploy

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -8,17 +8,19 @@ import {
 import { getFirestore } from 'firebase/firestore';
 import { getPerformance } from 'firebase/performance';
 
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY_VERCEL,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN_VERCEL,
-  projectId: 'pawtales-86bbb',
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET_VERCEL,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID_VERCEL,
-  appId: import.meta.env.FIREBASE_APP_ID_VERCEL,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID_VERCEL,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
