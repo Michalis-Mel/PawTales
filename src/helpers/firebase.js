012e61d4de6c47a8495cf8894ed4b144961a5aa5
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-11 17:23:49 +0200

    redeploy

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -8,8 +8,6 @@ import {
 import { getFirestore } from 'firebase/firestore';
 import { getPerformance } from 'firebase/performance';
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
