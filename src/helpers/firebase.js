de8692186a1c9770e4ab1a3ebba3ff3116cbd8a5
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-11 17:18:13 +0200

    added env file

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -14,13 +14,13 @@ import { getPerformance } from 'firebase/performance';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.FIREBASE_API_KEY_VERCEL,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN_VERCEL,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID_VERCEL,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET_VERCEL,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID_VERCEL,
+  appId: import.meta.env.FIREBASE_APP_ID_VERCEL,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID_VERCEL,
 };
 
 // Initialize Firebase
