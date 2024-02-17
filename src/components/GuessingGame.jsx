6707ec15bcc5254fca816c2d996344c6ee64b25d
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-17 17:27:41 +0200

    updated the game

--- src/components/GuessingGame.jsx
+++ src/components/GuessingGame.jsx
@@ -17,12 +17,12 @@ const GuessingGame = () => {
       setIsLoading(true);
       const img = new Image();
       img.src = animalsGame[currentIndex].image;
-      // img.onload = () => {
-      //   setCurrentAnimal(animalsGame[currentIndex]);
-      //   setSelectedOption(null);
-      //   setIsCorrect(null);
-      //   setIsLoading(false);
-      // };
+      img.onload = () => {
+        setCurrentAnimal(animalsGame[currentIndex]);
+        setSelectedOption(null);
+        setIsCorrect(null);
+        setIsLoading(false);
+      };
     }
   }, [currentIndex]);
 
