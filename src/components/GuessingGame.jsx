c2eea0f5c99aab054ec0937c9bfd7880cef6f987
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-17 16:58:31 +0200

    updated the game

--- src/components/GuessingGame.jsx
+++ src/components/GuessingGame.jsx
@@ -8,52 +8,83 @@ const GuessingGame = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState(null);
   const [isCorrect, setIsCorrect] = useState(null);
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
   useEffect(() => {
-    setCurrentAnimal(animalsGame[currentIndex]);
+    if (currentIndex < animalsGame.length) {
+      setCurrentAnimal(animalsGame[currentIndex]);
+    }
   }, [currentIndex]);
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
     setIsCorrect(option === currentAnimal.name);
     if (option === currentAnimal.name) {
-      setCurrentIndex((currentIndex + 1) % animalsGame.length);
+      setCurrentIndex(currentIndex + 1);
       setSelectedOption(null);
       setIsCorrect(null);
     }
   };
 
+  const startGame = () => {
+    setIsGameStarted(true);
+  };
+
+  const restartGame = () => {
+    setCurrentIndex(0);
+  };
+
   return (
-    <>
-      {currentAnimal && (
-        <motion.div
-          className='guessingGame'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 0.3 }}
-        >
-          <h1>Παιχνίδι Γνώσεων</h1>
-          <div className='game'>
-            <img src={currentAnimal.image} alt={currentAnimal.name} />
-            <h6>Βρες το ζώο της εικόνας</h6>
-            <div className='game_btns'>
-              {currentAnimal.options.map((option, index) => (
-                <div className='btn_con' key={index}>
-                  <button
-                    onClick={() => handleOptionClick(option)}
-                    className={` ${
-                      selectedOption === option && !isCorrect ? 'wrong' : ''
-                    } `}
-                  >
-                    {option}
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-        </motion.div>
-      )}
-    </>
+    <motion.div
+      className='guessingGame'
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 0.3 }}
+      viewport={{ once: true }}
+    >
+      <h1>Παιχνίδι Γνώσεων</h1>
+      <div className='game'>
+        {!isGameStarted ? (
+          <>
+            <h6>
+              Δείξε μας πόσο καλά γνωρίζεις τα ζώα που συναντάς στις ιστορίες
+              μας!
+            </h6>
+            <button onClick={startGame} className='start'>
+              Ξεκίνα το Παιχνίδι
+            </button>
+          </>
+        ) : currentIndex < animalsGame.length ? (
+          currentAnimal && (
+            <>
+              <img src={currentAnimal.image} alt={currentAnimal.name} />
+              <h6>Βρες το ζώο της εικόνας</h6>
+              <div className='game_btns'>
+                {currentAnimal.options.map((option, index) => (
+                  <div className='btn_con' key={index}>
+                    <button
+                      onClick={() => handleOptionClick(option)}
+                      className={` ${
+                        selectedOption === option && !isCorrect ? 'wrong' : ''
+                      } `}
+                    >
+                      {option}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </>
+          )
+        ) : (
+          <>
+            <h6>Συγχαρητήρια! Έχετε ολοκληρώσει το παιχνίδι.</h6>
+            <button onClick={restartGame} className='restart'>
+              Ξαναπαίξτε
+            </button>
+          </>
+        )}
+      </div>
+    </motion.div>
   );
 };
 
