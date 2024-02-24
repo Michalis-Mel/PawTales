44c3726f9203efab34bbd1d7df848f01c44abb91
Michael Meliopoulos <mixalismeliop@gmail.com>
2024-02-24 16:38:46 +0200

    update firestore

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -5,14 +5,12 @@ import {
   GoogleAuthProvider,
   FacebookAuthProvider,
 } from 'firebase/auth';
-import { getFirestore, initializeFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getPerformance } from 'firebase/performance';
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -25,19 +23,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const perf = getPerformance(app);
-// export const firestore = getFirestore(app);
-export const firestore = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-});
-
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
-
-// Create a GoogleAuthProvider instance
+export const auth = getAuth();
 export const googleProvider = new GoogleAuthProvider();
-
-// Create a AppleAuthProvider instance
 export const facebookProvider = new FacebookAuthProvider();
+export const firestore = getFirestore();
+export const perf = getPerformance();
 
 export default app;
